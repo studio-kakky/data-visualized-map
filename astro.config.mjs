5d75c891aa7bbdd58e54e86c57de559cf746b76e
studio-kakky <info@studio-kakky.com>
2022-09-01 17:15:28 +0900

    remove css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["mapbox-gl"],
+      },
+    },
+  },
 });
