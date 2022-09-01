9d20fde57f9fce4f80a240c132c7b7b45a3cac5c
studio-kakky <info@studio-kakky.com>
2022-09-01 16:24:53 +0900

    cssをローカルに

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,7 @@
 import { DeckGL } from '@deck.gl/react';
 import { ColumnLayer } from '@deck.gl/layers';
 import { Map } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import './mapbox-gl.css';
 import { useEffect, useState } from 'react';
 import { meshWeatherUsecase } from '../../shared/usecases/meshWeather/meshWeather';
 import { AppDateTime } from '../../shared/models/date/AppDateTime';
