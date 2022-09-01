eb4bc8d9221e423ec90fe21fafc09a6bd1e421ac
studio-kakky <info@studio-kakky.com>
2022-09-01 17:28:19 +0900

    add mapbox gl

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  vite: {
-    build: {
-      rollupOptions: {
-        external: ["mapbox-gl"],
-      },
-    },
-  },
 });
