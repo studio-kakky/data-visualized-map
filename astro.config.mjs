21e02b66d4c3017bc0641641861e068ad6d6bc94
studio-kakky <info@studio-kakky.com>
2022-09-01 16:39:28 +0900

    external mapbox-gl を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,15 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "entry.[hash].js",
+          chunkFileNames: "chunks/chunk.[hash].js",
+          assetFileNames: "assets/asset.[hash][extname]",
+        },
+      },
+    },
+  },
 });
